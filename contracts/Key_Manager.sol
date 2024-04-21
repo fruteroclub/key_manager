84c34ad968cc514b385c02f3c567fdef45161445
ArturVargas <arturvargas9226@gmail.com>
2024-04-21 02:29:20 -0600

    fix

--- contracts/Key_Manager.sol
+++ contracts/Key_Manager.sol
@@ -6,10 +6,11 @@ import "@fhenixprotocol/contracts/FHE.sol";
 contract Key_Manager {
     mapping(address => euint32) public _encKeys;
 
-    function add_key(address _address, euint32 _key) public {
+    function add_key(address _address, inEuint32 calldata _key) public returns (euint32) {
         require(_address != address(0x0), "Invalid address");
 
-        _encKeys[_address] = _key;
+        _encKeys[_address] = FHE.asEuint32(_key);
+        return _encKeys[_address];
     }
 
     function get_key(address _address) public view returns (euint32) {
